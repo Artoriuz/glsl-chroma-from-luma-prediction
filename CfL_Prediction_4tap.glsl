cc15498fe4fb88f13f91e80cafc5daccde6d7953
Artoriuz <joao00999@gmail.com>
2023-10-02 18:56:22 -0300

    adding spatial chroma interpolation and removing the dependency, making the shader 100% independent (and much faster)

--- CfL_Prediction_4tap.glsl
+++ CfL_Prediction_4tap.glsl
@@ -24,6 +24,8 @@
 //!BIND LUMA
 //!BIND HOOKED
 //!SAVE LUMA_LOWRES
+//!WIDTH CHROMA.w
+//!HEIGHT CHROMA.h
 //!WHEN CHROMA.w LUMA.w <
 //!DESC Chroma From Luma Prediction (Downscaling Luma)
 
@@ -53,18 +55,34 @@ vec4 hook() {
     return output_pix;
 }
 
-//!HOOK NATIVE
-//!BIND NATIVE
+//!HOOK CHROMA
 //!BIND CHROMA
+//!BIND LUMA
 //!BIND LUMA_LOWRES
 //!WHEN CHROMA.w LUMA.w <
+//!WIDTH LUMA.w
+//!HEIGHT LUMA.h
 //!OFFSET ALIGN
 //!DESC Chroma From Luma Prediction (4-tap, Upscaling Chroma)
 
+float comp_wd(vec2 distance) {
+    float d = length(distance);
+    if (d < 1.0) {
+        return (6.0 + d * d * (-15.0 + d * 9.0)) / 6.0;
+    } else if (d < 2.0) {
+        return (12.0 + d * (-24.0 + d * (15.0 + d * -3.0))) / 6.0;
+    } else {
+        return 0.0;
+    }
+}
+
 vec4 hook() {
-    vec4 output_pix = NATIVE_texOff(0.0);
+    vec4 output_pix = vec4(0.0, 0.0, 0.0, 1.0);
+    float luma_zero = LUMA_texOff(0.0).x;
+
     vec2 pp = CHROMA_pos * CHROMA_size - vec2(0.5);
     vec2 fp = floor(pp);
+    pp -= fp;
 
     vec2 chroma_pixels[12];
     chroma_pixels[0] = CHROMA_tex(vec2((fp + vec2(0.5, -0.5)) * CHROMA_pt)).xy;
@@ -94,17 +112,44 @@ vec4 hook() {
     luma_pixels[10] = LUMA_LOWRES_tex(vec2((fp + vec2(0.5, 2.5) ) * CHROMA_pt)).x;
     luma_pixels[11] = LUMA_LOWRES_tex(vec2((fp + vec2(1.5, 2.5) ) * CHROMA_pt)).x;
 
-    // vec2 chroma_min = vec2(1e8);
-    // chroma_min = min(chroma_min, chroma_pixels[3]);
-    // chroma_min = min(chroma_min, chroma_pixels[4]);
-    // chroma_min = min(chroma_min, chroma_pixels[7]);
-    // chroma_min = min(chroma_min, chroma_pixels[8]);
+    vec2 chroma_min = vec2(1e8);
+    chroma_min = min(chroma_min, chroma_pixels[3]);
+    chroma_min = min(chroma_min, chroma_pixels[4]);
+    chroma_min = min(chroma_min, chroma_pixels[7]);
+    chroma_min = min(chroma_min, chroma_pixels[8]);
     
-    // vec2 chroma_max = vec2(1e-8);
-    // chroma_max = max(chroma_max, chroma_pixels[3]);
-    // chroma_max = max(chroma_max, chroma_pixels[4]);
-    // chroma_max = max(chroma_max, chroma_pixels[7]);
-    // chroma_max = max(chroma_max, chroma_pixels[8]);
+    vec2 chroma_max = vec2(1e-8);
+    chroma_max = max(chroma_max, chroma_pixels[3]);
+    chroma_max = max(chroma_max, chroma_pixels[4]);
+    chroma_max = max(chroma_max, chroma_pixels[7]);
+    chroma_max = max(chroma_max, chroma_pixels[8]);
+
+    float wd[12];
+    wd[0]  = comp_wd(vec2( 0.0,-1.0) - pp);
+    wd[1]  = comp_wd(vec2( 1.0,-1.0) - pp);
+    wd[2]  = comp_wd(vec2(-1.0, 0.0) - pp);
+    wd[3]  = comp_wd(vec2( 0.0, 0.0) - pp);
+    wd[4]  = comp_wd(vec2( 1.0, 0.0) - pp);
+    wd[5]  = comp_wd(vec2( 2.0, 0.0) - pp);
+    wd[6]  = comp_wd(vec2(-1.0, 1.0) - pp);
+    wd[7]  = comp_wd(vec2( 0.0, 1.0) - pp);
+    wd[8]  = comp_wd(vec2( 1.0, 1.0) - pp);
+    wd[9]  = comp_wd(vec2( 2.0, 1.0) - pp);
+    wd[10] = comp_wd(vec2( 0.0, 2.0) - pp);
+    wd[11] = comp_wd(vec2( 1.0, 2.0) - pp);
+
+    float wt = 0.0;
+    for (int i = 0; i < 12; i++) {
+        wt += wd[i];
+    }
+
+    vec2 ct = vec2(0.0);
+    for (int i = 0; i < 12; i++) {
+        ct += wd[i] * chroma_pixels[i];
+    }
+
+    vec2 chroma_spatial = ct / wt;
+    chroma_spatial = clamp(chroma_spatial, chroma_min, chroma_max);
 
     float luma_avg_4 = 0.0;
     luma_avg_4 += luma_pixels[3];
@@ -135,7 +180,7 @@ vec4 hook() {
     vec2 alpha_4 = luma_chroma_cov_4 / max(luma_var_4, 1e-6);
     vec2 beta_4 = chroma_avg_4 - alpha_4 * luma_avg_4;
 
-    vec2 chroma_pred_4 = alpha_4 * output_pix.x + beta_4;
+    vec2 chroma_pred_4 = alpha_4 * luma_zero + beta_4;
     chroma_pred_4 = clamp(chroma_pred_4, 0.0, 1.0);
 
     float luma_avg_12 = 0.0;
@@ -168,10 +213,10 @@ vec4 hook() {
     vec2 corr = abs(luma_chroma_cov_12 / max(sqrt(luma_var_12 * chroma_var_12), 1e-6));
     corr = clamp(corr, 0.0, 1.0);
 
-    output_pix.yz = mix(output_pix.yz, chroma_pred_4, pow(corr, vec2(2.0)) / 2.0);
+    output_pix.xy = mix(chroma_spatial, chroma_pred_4, pow(corr, vec2(2.0)) / 2.0);
 
     // Replace this with chroma_min and chroma_max if you want AR
     // output_pix.yz = clamp(output_pix.yz, chroma_min, chroma_max);
-    output_pix.yz = clamp(output_pix.yz, 0.0, 1.0);
+    output_pix.xy = clamp(output_pix.xy, 0.0, 1.0);
     return  output_pix;
 }
