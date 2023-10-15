f5c31d0f009b408fca3605bbe30d28900e1ef999
Joao Chrisostomo <joao00999@gmail.com>
2023-10-15 09:04:56 -0300

    Standalone 12-tap shader removed. Merged with main variant.

--- CfL_Prediction.glsl
+++ CfL_Prediction.glsl
@@ -84,13 +84,15 @@ vec4 hook() {
 //!OFFSET ALIGN
 //!DESC Chroma From Luma Prediction (Upscaling Chroma)
 
+#define USE_4_TAP_REGRESSION 1
+
 float comp_wd(vec2 distance) {
     float d2 = min(pow(length(distance), 2.0), 4.0);
     return (25.0 / 16.0 * pow(2.0 / 5.0 * d2 - 1.0, 2.0) - (25.0 / 16.0 - 1.0)) * pow(1.0 / 4.0 * d2 - 1.0, 2.0);
 }
 
 vec4 hook() {
-    float division_limit = 1e-3;
+    float division_limit = 1e-4;
 
     vec4 output_pix = vec4(0.0, 0.0, 0.0, 1.0);
     float luma_zero = LUMA_texOff(0.0).x;
@@ -166,38 +168,6 @@ vec4 hook() {
     vec2 chroma_spatial = ct / wt;
     chroma_spatial = mix(chroma_spatial, clamp(chroma_spatial, chroma_min, chroma_max), 0.75);
 
-    float luma_avg_4 = 0.0;
-    luma_avg_4 += luma_pixels[3];
-    luma_avg_4 += luma_pixels[4];
-    luma_avg_4 += luma_pixels[7];
-    luma_avg_4 += luma_pixels[8];
-    luma_avg_4 /= 4.0;
-
-    float luma_var_4 = 0.0;
-    luma_var_4 += pow(luma_pixels[3] - luma_avg_4, 2.0);
-    luma_var_4 += pow(luma_pixels[4] - luma_avg_4, 2.0);
-    luma_var_4 += pow(luma_pixels[7] - luma_avg_4, 2.0);
-    luma_var_4 += pow(luma_pixels[8] - luma_avg_4, 2.0);
-
-    vec2 chroma_avg_4 = vec2(0.0);
-    chroma_avg_4 += chroma_pixels[3];
-    chroma_avg_4 += chroma_pixels[4];
-    chroma_avg_4 += chroma_pixels[7];
-    chroma_avg_4 += chroma_pixels[8];
-    chroma_avg_4 /= 4.0;
-
-    vec2 luma_chroma_cov_4 = vec2(0.0);
-    luma_chroma_cov_4 += (luma_pixels[3] - luma_avg_4) * (chroma_pixels[3] - chroma_avg_4);
-    luma_chroma_cov_4 += (luma_pixels[4] - luma_avg_4) * (chroma_pixels[4] - chroma_avg_4);
-    luma_chroma_cov_4 += (luma_pixels[7] - luma_avg_4) * (chroma_pixels[7] - chroma_avg_4);
-    luma_chroma_cov_4 += (luma_pixels[8] - luma_avg_4) * (chroma_pixels[8] - chroma_avg_4);
-
-    vec2 alpha_4 = luma_chroma_cov_4 / max(luma_var_4, division_limit);
-    vec2 beta_4 = chroma_avg_4 - alpha_4 * luma_avg_4;
-
-    vec2 chroma_pred_4 = alpha_4 * luma_zero + beta_4;
-    chroma_pred_4 = clamp(chroma_pred_4, 0.0, 1.0);
-
     float luma_avg_12 = 0.0;
     for(int i = 0; i < 12; i++) {
         luma_avg_12 += luma_pixels[i];
@@ -233,11 +203,46 @@ vec4 hook() {
 
     vec2 chroma_pred_12 = alpha_12 * luma_zero + beta_12;
     chroma_pred_12 = clamp(chroma_pred_12, 0.0, 1.0);
-
-    chroma_pred_4 = mix(chroma_spatial, chroma_pred_4, pow(corr, vec2(2.0)) / 2.0);
     chroma_pred_12 = mix(chroma_spatial, chroma_pred_12, pow(corr, vec2(2.0)) / 2.0);
-    output_pix.xy = mix(chroma_pred_4, chroma_pred_12, 0.5);
 
+#if (USE_4_TAP_REGRESSION == 1)
+    float luma_avg_4 = 0.0;
+    luma_avg_4 += luma_pixels[3];
+    luma_avg_4 += luma_pixels[4];
+    luma_avg_4 += luma_pixels[7];
+    luma_avg_4 += luma_pixels[8];
+    luma_avg_4 /= 4.0;
+
+    float luma_var_4 = 0.0;
+    luma_var_4 += pow(luma_pixels[3] - luma_avg_4, 2.0);
+    luma_var_4 += pow(luma_pixels[4] - luma_avg_4, 2.0);
+    luma_var_4 += pow(luma_pixels[7] - luma_avg_4, 2.0);
+    luma_var_4 += pow(luma_pixels[8] - luma_avg_4, 2.0);
+
+    vec2 chroma_avg_4 = vec2(0.0);
+    chroma_avg_4 += chroma_pixels[3];
+    chroma_avg_4 += chroma_pixels[4];
+    chroma_avg_4 += chroma_pixels[7];
+    chroma_avg_4 += chroma_pixels[8];
+    chroma_avg_4 /= 4.0;
+
+    vec2 luma_chroma_cov_4 = vec2(0.0);
+    luma_chroma_cov_4 += (luma_pixels[3] - luma_avg_4) * (chroma_pixels[3] - chroma_avg_4);
+    luma_chroma_cov_4 += (luma_pixels[4] - luma_avg_4) * (chroma_pixels[4] - chroma_avg_4);
+    luma_chroma_cov_4 += (luma_pixels[7] - luma_avg_4) * (chroma_pixels[7] - chroma_avg_4);
+    luma_chroma_cov_4 += (luma_pixels[8] - luma_avg_4) * (chroma_pixels[8] - chroma_avg_4);
+
+    vec2 alpha_4 = luma_chroma_cov_4 / max(luma_var_4, division_limit);
+    vec2 beta_4 = chroma_avg_4 - alpha_4 * luma_avg_4;
+
+    vec2 chroma_pred_4 = alpha_4 * luma_zero + beta_4;
+    chroma_pred_4 = clamp(chroma_pred_4, 0.0, 1.0);
+    chroma_pred_4 = mix(chroma_spatial, chroma_pred_4, pow(corr, vec2(2.0)) / 2.0);
+
+    output_pix.xy = mix(chroma_pred_4, chroma_pred_12, 0.5);
+#else
+    output_pix.xy = chroma_pred_12;
+#endif
     // Replace this with chroma_min and chroma_max if you want AR
     // output_pix.yz = clamp(output_pix.yz, chroma_min, chroma_max);
     output_pix.xy = clamp(output_pix.xy, 0.0, 1.0);
--- README.md
+++ README.md
@@ -5,9 +5,8 @@ The shaders implement chroma upscaling based on the closed least squares solutio
 Since a simple linear regression obviously doesn't take into account pixel distance, the prediction is mixed with the output of a normal resampling filter based on how high the correlation between luma and chroma is.
 
 The repo contains 3 variants of the shader:
-- `CfL_Prediction.glsl`: Main variant, attempts to recreate as much detail as possible without introducing too many artifacts. 
+- `CfL_Prediction.glsl`: Main variant, attempts to recreate as much detail as possible without introducing too many artifacts. You can disable the 4-tap regression with `USE_4_TAP_REGRESSION 0` to reduce the amount of artifacts created by the shader, although it'll also make it softer.
 - `CfL_Prediction_4tap.glsl`: A more aggressive variant that only uses the 4 nearest data pairs in its linear regression. Can sometimes produce better fine detail but it's also much less stable (more artifacts).
-- `CfL_Prediction_12tap.glsl`: A less aggressive variant without the smaller linear regression at all. This is the safest option if you want to avoid the worst artifacts, but it can also look a bit softer sometimes.
 
 The shaders are experimental and minor improvements are being made over time. If you have any suggestions, feel free to send them.
 
